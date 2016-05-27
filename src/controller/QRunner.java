161d06c5f4b999d25858404ab9754e037ac68863
Sam Montoya <smon7958@740s-pl-332.local>
2016-05-27 12:46:04 -0600

    Added to Repo

---
+++ src/controller/QRunner.java
@@ -0,0 +1,6 @@
+package controller;
+
+public class QRunner
+	{
+		public static void main(String[]args)	{	new QController();		}
+	}	
---
+++ src/model/Customer.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Customer
+	{
+		private boolean hasOrder;
+		private String name;
+		private double total;
+		
+		public Customer()
+		{
+			hasOrder = false;
+			name = "Brettly Clawfield";
+			total = 6.75;
+		}
+	}
